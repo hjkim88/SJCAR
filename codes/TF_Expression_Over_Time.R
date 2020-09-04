28f45cf3a172f1288891d0de2d8d7f2d96e4c822
hjkim88 <firadazer@gmail.com>
2020-09-04 00:42:42 -0500

    in implementation.

--- codes/TF_Expression_Over_Time.R
+++ codes/TF_Expression_Over_Time.R
@@ -42,6 +42,7 @@ tf_expression <- function(Seurat_RObj_path="./data/JCC212_21Feb2020Aggreg_regres
   
   ### rownames in the meta.data should be in the same order as colnames in the counts
   Seurat_Obj@meta.data <- Seurat_Obj@meta.data[colnames(Seurat_Obj@assays$RNA@counts),]
+  print(identical(rownames(Seurat_Obj@meta.data), colnames(Seurat_Obj@assays$RNA@counts)))
   
   ### active assay = "RNA"
   Seurat_Obj@active.assay <- "RNA"
@@ -49,39 +50,51 @@ tf_expression <- function(Seurat_RObj_path="./data/JCC212_21Feb2020Aggreg_regres
   ### check whether the orders are the same
   print(identical(names(Idents(object = Seurat_Obj)), rownames(Seurat_Obj@meta.data)))
   
-  
-  ### set Idents of the object before performing DE analysis
-  Seurat_Obj <- SetIdent(object = Seurat_Obj,
-                         cells = rownames(Seurat_Obj@meta.data),
-                         value = Seurat_Obj@meta.data$Time)
-  
   ### I tried to run FindAllMarkers() but the sample size is too large,
-  ### so I divide the samples for each time point then randomly chose from the others to compare
+  ### so I divide the samples for each time point then randomly chose from the others for the comparison
+  
+  ### there are too many cells so selet the fixed number of cells for the comparison
+  sample_num <- 1000
   
   ### for each time point run DE analysis vs the rest
   tp_of_interest <- setdiff(levels(Seurat_Obj@meta.data$TimeF), c("PreTrans", "Wk-1", "Wk0"))
+  de_list <- vector("list", length(tp_of_interest))
+  names(de_list) <- tp_of_interest
   for(tp in tp_of_interest) {
     
     ### choose random samples from the rest
     set.seed(1234)
-    ctrl_grp_idx <- sample(which(Seurat_Obj@meta.data$Time != tp),
-                           length(which(Seurat_Obj@meta.data$Time == tp)))
+    exp_grp_idx <- which(Seurat_Obj@meta.data$Time == tp)
+    if(length(exp_grp_idx) > sample_num) {
+      exp_grp_idx <- sample(exp_grp_idx, sample_num)
+      ctrl_grp_idx <- sample(which(Seurat_Obj@meta.data$Time != tp), sample_num)
+    } else {
+      ctrl_grp_idx <- sample(which(Seurat_Obj@meta.data$Time != tp), length(exp_grp_idx))
+    }
     
-    ###
-    de_result <- FindMarkers()
+    ### make new idents
+    new_idents <- rep("NA", nrow(Seurat_Obj@meta.data))
+    new_idents[exp_grp_idx] <- "EXP"
+    new_idents[ctrl_grp_idx] <- "CTRL"
     
+    ### set Idents of the object before performing DE analysis
+    Seurat_Obj <- SetIdent(object = Seurat_Obj,
+                           cells = rownames(Seurat_Obj@meta.data),
+                           value = new_idents)
+    
+    ### run DE analysis
+    de_list[[tp]] <- FindMarkers(object = Seurat_Obj,
+                                 ident.1 = "EXP",
+                                 ident.2 = "CTRL",
+                                 test.use = "DESeq2",
+                                 logfc.threshold = 0,
+                                 min.pct = 0.1)
     
   }
   
+  ### Combine the DE genes using Fisher's method
   
-  
-  
-  ### run DE analysis based on all the time points
-  all_de_result <- FindAllMarkers(object = Seurat_Obj, test.use = "DESeq2",
-                                  logfc.threshold = 0, min.pct = 0.1)
-  
-  
-  
+  ### slingshot trajectory associated genes?
   
   
   
