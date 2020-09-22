cfd97208eb81bb7569d6584bfac82c3871e093b2
hjkim88 <firadazer@gmail.com>
2020-09-22 15:15:16 -0500

    Skeleton of NEW TCR data Testing.

---
+++ codes/Examine_New_TCR_Data.R
@@ -0,0 +1,30 @@
+###
+#   File name : Examine_New_TCR_Data.R
+#   Author    : Hyunjin Kim
+#   Date      : Sep 22, 2020
+#   Email     : hyunjin.kim@stjude.org
+#   Purpose   : Now we have new TCR data. Compare it to the old TCR data and
+#               examine how many more lineages can be made with the new data.
+#
+#   Instruction
+#               1. Source("Examine_New_TCR_Data.R")
+#               2. Run the function "examine_new_tcr_data" - specify the input directory and the output directory
+#               3. The results will be generated under the output directory
+#
+#   Example
+#               > source("The_directory_of_Examine_New_TCR_Data.R/Examine_New_TCR_Data.R")
+#               > examine_new_tcr_data(new_TCR_dir="C:/Users/hkim8/SJ/SJCAR19/TCRredo/",
+#                                      old_TCR_dir="C:/Users/hkim8/SJ/SJCAR19/TCRoutputs/",
+#                                      outputDir="./results/")
+###
+
+examine_new_tcr_data <- function(new_TCR_dir="C:/Users/hkim8/SJ/SJCAR19/TCRredo/",
+                                 old_TCR_dir="C:/Users/hkim8/SJ/SJCAR19/TCRoutputs/",
+                                 outputDir="./results/") {
+  
+  
+  
+  
+  
+  
+}
