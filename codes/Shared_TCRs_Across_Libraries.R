6de26636a8ee8b105350599272cd3a2e4f442056
hjkim88 <firadazer@gmail.com>
2020-10-02 23:13:39 -0500

    Skeleton of Shared_TCRs_Across_Libraries.R.

---
+++ codes/Shared_TCRs_Across_Libraries.R
@@ -0,0 +1,25 @@
+###
+#   File name : Shared_TCRs_Across_Libraries.R
+#   Author    : Hyunjin Kim
+#   Date      : Sep 22, 2020
+#   Email     : hyunjin.kim@stjude.org
+#   Purpose   : We would like to know how many TCRs are shared across libraries.
+#
+#   Instruction
+#               1. Source("Shared_TCRs_Across_Libraries.R")
+#               2. Run the function "shared_tcrs" - specify the input directory and the output directory
+#               3. The results will be generated under the output directory
+#
+#   Example
+#               > source("The_directory_of_Shared_TCRs_Across_Libraries.R/Shared_TCRs_Across_Libraries.R")
+#               > shared_tcrs(Seurat_RObj_path="./data/JCC212_21Feb2020Aggreg_regress_TCR_clonotyped_PROTO2.Robj",
+#                             new_TCR_dir="C:/Users/hkim8/SJ/SJCAR19/TCRredo/",
+#                             outputDir="./results/")
+###
+
+shared_tcrs <- function() {
+  
+  
+  
+    
+}
