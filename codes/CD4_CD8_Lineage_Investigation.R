f5bc9de38a6b27c07dd6c7ef7bee5fc466ce5b53
hjkim88 <firadazer@gmail.com>
2020-11-08 23:42:49 -0600

    Annotate CD4/CD8 to the cells and identify CD4/CD8 - associated lineages.

---
+++ codes/CD4_CD8_Lineage_Investigation.R
@@ -0,0 +1,92 @@
+###
+#   File name : CD4_CD8_Lineage_Investigation.R
+#   Author    : Hyunjin Kim
+#   Date      : Nov 8, 2020
+#   Email     : hyunjin.kim@stjude.org
+#   Purpose   : Annotate CD4/CD8 to the cells and identify CD4/CD8 - associated lineages.
+#
+#   Instruction
+#               1. Source("CD4_CD8_Lineage_Investigation.R")
+#               2. Run the function "cd4_cd8_investigation" - specify the input file path and the output directory
+#               3. The results will be generated under the output directory
+#
+#   Example
+#               > source("The_directory_of_CD4_CD8_Lineage_Investigation.R/CD4_CD8_Lineage_Investigation.R")
+#               > cd4_cd8_investigation(Seurat_RObj_path="Z:/ResearchHome/ResearchHomeDirs/thomagrp/common/Hyunjin/JCC212_SJCAR19/SJCAR19_Oct2020_Seurat_Obj3.RDS",
+#                                       clonotype_lineage_info_path="Z:/ResearchHome/ResearchHomeDirs/thomagrp/common/Hyunjin/JCC212_SJCAR19/SJCAR19_Clonotype_Lineages.RDS",
+#                                       outputDir="Z:/ResearchHome/ResearchHomeDirs/thomagrp/common/Hyunjin/JCC212_SJCAR19/new_results/")
+###
+
+cd4_cd8_investigation <- function(Seurat_RObj_path="./data/SJCAR19_Oct2020_Seurat_Obj3.RDS",
+                                  clonotype_lineage_info_path="./data/SJCAR19_Clonotype_Lineages.RDS",
+                                  outputDir="./results/New/") {
+  
+  ### load libraries
+  if(!require(Seurat, quietly = TRUE)) {
+    install.packages("Seurat")
+    require(Seurat, quietly = TRUE)
+  }
+  if(!require(ggplot2, quietly = TRUE)) {
+    install.packages("ggplot2")
+    require(ggplot2, quietly = TRUE)
+  }
+  if(!require(xlsx, quietly = TRUE)) {
+    install.packages("xlsx")
+    require(xlsx, quietly = TRUE)
+  }
+  if(!require(SingleR, quietly = TRUE)) {
+    if (!requireNamespace("BiocManager", quietly = TRUE))
+      install.packages("BiocManager")
+    BiocManager::install("SingleR")
+    require(SingleR, quietly = TRUE)
+  }
+  if(!require(SingleCellExperiment, quietly = TRUE)) {
+    if (!requireNamespace("BiocManager", quietly = TRUE))
+      install.packages("BiocManager")
+    BiocManager::install("SingleCellExperiment")
+    require(SingleCellExperiment, quietly = TRUE)
+  }
+  
+  ### create outputDir
+  dir.create(outputDir, showWarnings = FALSE, recursive = TRUE)
+  
+  ### load Seurat object
+  Seurat_Obj <- readRDS(Seurat_RObj_path)
+  
+  ### rownames in the meta.data should be in the same order as colnames in the counts
+  Seurat_Obj@meta.data <- Seurat_Obj@meta.data[colnames(Seurat_Obj@assays$RNA@counts),]
+  print(identical(rownames(Seurat_Obj@meta.data), colnames(Seurat_Obj@assays$RNA@counts)))
+  
+  ### active assay = "RNA"
+  Seurat_Obj@active.assay <- "RNA"
+  
+  ### check whether the orders are the same
+  print(identical(names(Idents(object = Seurat_Obj)), rownames(Seurat_Obj@meta.data)))
+  
+  ### set time points
+  total_time_points <- c("PreTrans", "PreTransB", "Wk-1", "Wk-1Run1", "Wk-1Run2", "Wk0", "GMP", "GMP-redo",
+                         "Wk1", "Wk1b", "Wk2", "Wk3", "Wk4", "Wk6", "Wk8", "3mo", "6mo", "9mo")
+  
+  
+  NHD <- NovershternHematopoieticData()    
+  
+  
+  
+  C1_GMP_sub_SSE <- as.SingleCellExperiment(C1_GMP_sub, assay = "RNA")
+  
+  
+  
+  C1_GMP_sub_NHD.main <- SingleR(test = C1_GMP_sub_SSE, ref = list(NHD), labels = list(NHD$label.main ))
+  table(C1_GMP_sub_NHD.main$labels)
+  #save(C1_GMP_sub_NHD.main,file = "C1_GMP_sub_NHD.main.Robj")
+  
+  
+  
+  C1_GMP_sub_NHD.fine <- SingleR(test = C1_GMP_sub_SSE, ref = list(NHD), labels = list(NHD$label.fine ))
+  table(C1_GMP_sub_NHD.fine$labels)
+  #save(C1_GMP_sub_NHD.fine,file = "C1_GMP_sub_NHD.fine.Robj")
+  
+  
+  
+  
+}
