1d95bbfef5ad437559a464a02e18cc1931685c79
hjkim88 <firadazer@gmail.com>
2020-11-11 18:46:58 -0600

    CD4/CD8 annotation part completed.

--- codes/CD4_CD8_Lineage_Investigation.R
+++ codes/CD4_CD8_Lineage_Investigation.R
@@ -12,14 +12,14 @@
 #
 #   Example
 #               > source("The_directory_of_CD4_CD8_Lineage_Investigation.R/CD4_CD8_Lineage_Investigation.R")
-#               > cd4_cd8_investigation(Seurat_RObj_path="Z:/ResearchHome/ResearchHomeDirs/thomagrp/common/Hyunjin/JCC212_SJCAR19/SJCAR19_Oct2020_Seurat_Obj3.RDS",
-#                                       clonotype_lineage_info_path="Z:/ResearchHome/ResearchHomeDirs/thomagrp/common/Hyunjin/JCC212_SJCAR19/SJCAR19_Clonotype_Lineages.RDS",
-#                                       outputDir="Z:/ResearchHome/ResearchHomeDirs/thomagrp/common/Hyunjin/JCC212_SJCAR19/new_results/")
+#               > cd4_cd8_investigation(Seurat_RObj_path="Z:/ResearchHome/ResearchHomeDirs/thomagrp/common/Hyunjin/JCC212_SJCAR19/NEW_SJCAR_SEURAT_OBJ/SJCAR19_Oct2020_Seurat_Obj.RDS",
+#                                       clonotype_lineage_info_path="Z:/ResearchHome/ResearchHomeDirs/thomagrp/common/Hyunjin/JCC212_SJCAR19/NEW_SJCAR_SEURAT_OBJ/SJCAR19_Clonotype_Lineages.RDS",
+#                                       outputDir="Z:/ResearchHome/ResearchHomeDirs/thomagrp/common/Hyunjin/JCC212_SJCAR19/new_results2/")
 ###
 
-cd4_cd8_investigation <- function(Seurat_RObj_path="./data/SJCAR19_Oct2020_Seurat_Obj3.RDS",
-                                  clonotype_lineage_info_path="./data/SJCAR19_Clonotype_Lineages.RDS",
-                                  outputDir="./results/New/") {
+cd4_cd8_investigation <- function(Seurat_RObj_path="./data/NEW_SJCAR_SEURAT_OBJ/SJCAR19_Oct2020_Seurat_Obj.RDS",
+                                  clonotype_lineage_info_path="./data/NEW_SJCAR_SEURAT_OBJ/SJCAR19_Clonotype_Lineages.RDS",
+                                  outputDir="./results/New2/") {
   
   ### load libraries
   if(!require(Seurat, quietly = TRUE)) {
@@ -67,27 +67,87 @@ cd4_cd8_investigation <- function(Seurat_RObj_path="./data/SJCAR19_Oct2020_Seura
   total_time_points <- c("PreTrans", "PreTransB", "Wk-1", "Wk-1Run1", "Wk-1Run2", "Wk0", "GMP", "GMP-redo",
                          "Wk1", "Wk1b", "Wk2", "Wk3", "Wk4", "Wk6", "Wk8", "3mo", "6mo", "9mo")
   
-  ### download the hematopoietic cell population data
-  NHD <- NovershternHematopoieticData()    
+  ### CD4/CD8 by expression
+  exp_quotient <- Seurat_Obj@assays$RNA@counts["CD4",] / Seurat_Obj@assays$RNA@counts["CD8A",]
+  cd4_idx <- which(exp_quotient > 1)
+  cd8_idx <- which(exp_quotient < 1)
+  both_idx <- which(exp_quotient == 1)
+  Seurat_Obj@meta.data$CD4_CD8_by_Exp <- NA
+  Seurat_Obj@meta.data$CD4_CD8_by_Exp[cd4_idx] <- "CD4"
+  Seurat_Obj@meta.data$CD4_CD8_by_Exp[cd8_idx] <- "CD8"
+  Seurat_Obj@meta.data$CD4_CD8_by_Exp[both_idx] <- "BOTH"
+  rm(exp_quotient)
+  gc()
   
+  ### download the hematopoietic cell population data
+  NHD <- NovershternHematopoieticData()
+  
+  ### set idents with the libary
   Seurat_Obj <- SetIdent(object = Seurat_Obj,
                          cells = rownames(Seurat_Obj@meta.data),
                          value = Seurat_Obj@meta.data$library)
-  subset_Seurat_Obj <- subset(Seurat_Obj, idents = "SJCAR19-05_GMP_GMP")
   
-  ### get gene expressions from the Seurat object
-  target_mat <- as.SingleCellExperiment(subset_Seurat_Obj, assay = "RNA")
+  ### for each library add annotations of CD4/CD8 using singleR
+  Seurat_Obj@meta.data$CD4_CD8_by_NHD_main <- NA
+  Seurat_Obj@meta.data$CD4_CD8_by_NHD_fine <- NA
+  for(lib in unique(Seurat_Obj@meta.data$library)) {
+    ### print progress
+    writeLines(paste(lib))
+    
+    ### get gene expressions from the subset of the Seurat object
+    target_mat <- as.SingleCellExperiment(subset(Seurat_Obj, idents = lib), assay = "RNA")
+    
+    ### get CD4/CD8 annotation using the main labels
+    NHD.main <- SingleR(test = target_mat, ref = list(NHD), labels = list(NHD$label.main))$labels
+    
+    ### get CD4/CD8 annotation using the fine labels
+    NHD.fine <- SingleR(test = target_mat, ref = list(NHD), labels = list(NHD$label.fine))$labels
+    
+    ### add annotions to the meta data
+    Seurat_Obj@meta.data[colnames(target_mat),"CD4_CD8_by_NHD_main"] <- NHD.main
+    Seurat_Obj@meta.data[colnames(target_mat),"CD4_CD8_by_NHD_fine"] <- NHD.fine
+    
+    gc()
+  }
   
-  ###
-  NHD.main <- SingleR(test = target_mat, ref = list(NHD), labels = list(NHD$label.main))
-  table(NHD.main$labels)
+  ### CD4/CD8 Consensus
+  Seurat_Obj@meta.data$CD4_CD8_by_Consensus <- NA
+  for(i in 1:nrow(Seurat_Obj@meta.data)) {
+    cd4_cnt <- 0
+    cd8_cnt <- 0
+    if(Seurat_Obj@meta.data$CD4_CD8_by_Exp == "CD4") {
+      cd4_cnt <- cd4_cnt + 1
+    } else if (Seurat_Obj@meta.data$CD4_CD8_by_Exp == "CD8") {
+      cd8_cnt <- cd8_cnt + 1
+    }
+    if(grepl("CD4", Seurat_Obj@meta.data$CD4_CD8_by_NHD_main[i], fixed = TRUE)) {
+      cd4_cnt <- cd4_cnt + 1
+    } else if(grepl("CD8", Seurat_Obj@meta.data$CD4_CD8_by_NHD_main[i], fixed = TRUE)) {
+      cd8_cnt <- cd8_cnt + 1
+    }
+    if(grepl("CD4", Seurat_Obj@meta.data$CD4_CD8_by_NHD_fine[i], fixed = TRUE)) {
+      cd4_cnt <- cd4_cnt + 1
+    } else if(grepl("CD8", Seurat_Obj@meta.data$CD4_CD8_by_NHD_fine[i], fixed = TRUE)) {
+      cd8_cnt <- cd8_cnt + 1
+    }
+    
+    ### decide with the consensus
+    if(cd4_cnt > cd8_cnt) {
+      Seurat_Obj@meta.data$CD4_CD8_by_Consensus[i] <- "CD4"
+    } else if(cd4_cnt < cd8_cnt) {
+      Seurat_Obj@meta.data$CD4_CD8_by_Consensus[i] <- "CD8"
+    } else if((cd4_cnt != 0) && (cd4_cnt == cd8_cnt)) {
+      Seurat_Obj@meta.data$CD4_CD8_by_Consensus[i] <- "BOTH"
+    }
+    
+    ### print progress
+    if(i %% 10000 == 0) {
+      writeLines(paste(i, "/", nrow(Seurat_Obj@meta.data)))
+    }
+  }
   
-  
-  
-  NHD.fine <- SingleR(test = target_mat, ref = list(NHD), labels = list(NHD$label.fine ))
-  table(C1_GMP_sub_NHD.fine$labels)
-  
-  
+  ### load clonotype lineages info
+  SJCAR19_Clonotype_Frequency <- readRDS(clonotype_lineage_info_path)
   
   
 }
