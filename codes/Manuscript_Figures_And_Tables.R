7911efb9b237a839bc18e40981c320f6abe64df3
hjkim88 <firadazer@gmail.com>
2021-04-18 23:17:12 -0500

    2. Number of lineages & clonotypes table CD4+, CD8+, CAR+, CAR-, per patient.

--- codes/Manuscript_Figures_And_Tables.R
+++ codes/Manuscript_Figures_And_Tables.R
@@ -128,7 +128,7 @@ manuscript_prep <- function(Seurat_RObj_path="./data/NEW_SJCAR_SEURAT_OBJ/SJCAR1
     
     ### load the file
     target_file <- read.xlsx2(file = paste0(px_result_dir, patient, "/car_clonotype_frequency_over_time_", patient, ".xlsx"),
-                              sheetIndex = 1, stringsAsFactors = FALSE, check.names = FALSE,
+                              sheetName = paste0("CARpos_Clonotype_Frequency_One_From_Each"), stringsAsFactors = FALSE, check.names = FALSE,
                               row.names = 1)
     
     ### numerize the table
@@ -275,7 +275,169 @@ manuscript_prep <- function(Seurat_RObj_path="./data/NEW_SJCAR_SEURAT_OBJ/SJCAR1
                    nrow = 3,
                    ncol = 3,
                    top = "")
-  ggsave(file = paste0(outputDir2, "Car+_Clonal_Tracing_9pxs_in_one.png"), g, width = 20, height = 15, dpi = 350)
+  ggsave(file = paste0(outputDir2, "Car+_Clonal_Tracing_9pxs_in_one.png"), g, width = 40, height = 15, dpi = 350)
+  
+  
+  ### 2. Number of lineages & clonotypes table CD4+, CD8+, CAR+, CAR-, per patient
+  ### (alpha only, beta only, one from each, strict)
+  
+  ### create outputDir
+  outputDir2 <- paste0(outputDir, "/2/")
+  dir.create(outputDir2, showWarnings = FALSE, recursive = TRUE)
+  
+  ### set table colnames & rownames
+  table_colnames <- c("Alpha", "Beta", "One_From_Each", "Strict")
+  clonotype_types <- c("clonotype_id_by_patient_alpha", "clonotype_id_by_patient_beta",
+                       "clonotype_id_by_patient_one_alpha_beta", "clonotype_id_by_patient")
+  names(table_colnames) <- clonotype_types
+  names(clonotype_types) <- table_colnames
+  table_rownames <- c("CD4+/CAR+ Clonotype #", "CD4+/CAR- Clonotype #",
+                      "CD8+/CAR+ Clonotype #", "CD8+/CAR- Clonotype #",
+                      "CD4+/CAR+ Lineage #", "CD4+/CAR- Lineage #",
+                      "CD8+/CAR+ Lineage #", "CD8+/CAR- Lineage #",
+                      "CD4+/CAR+ GMP Lineage #", "CD8+/CAR+ GMP Lineage #")
+  px_table_rownames <- as.vector(sapply(unique(Seurat_Obj@meta.data$px), function(x) paste(x, table_rownames)))
+  
+  ### make an empty table
+  result_table <- matrix(0, length(px_table_rownames), length(table_colnames))
+  colnames(result_table) <- table_colnames
+  rownames(result_table) <- px_table_rownames
+  
+  ### for each clonotype type to get lineages
+  total_lineages <- vector("list", length = length(table_colnames))
+  names(total_lineages) <- table_colnames
+  total_gmp_lineages <- vector("list", length = length(table_colnames))
+  names(total_gmp_lineages) <- table_colnames
+  for(type in table_colnames) {
+    total_lineages[[type]] <- NULL
+    total_gmp_lineages[[type]] <- NULL
+    ### run for each patient
+    for(patient in unique(Seurat_Obj@meta.data$px)) {
+      
+      ### print progress
+      writeLines(paste(patient))
+      
+      ### load the file
+      target_file <- read.xlsx2(file = paste0(px_result_dir, patient, "/car_clonotype_frequency_over_time_", patient, ".xlsx"),
+                                sheetName = paste0("CARpos_Clonotype_Frequency_", type), stringsAsFactors = FALSE, check.names = FALSE,
+                                row.names = 1)
+      
+      ### numerize the table
+      for(i in 1:ncol(target_file)) {
+        target_file[,i] <- as.numeric(target_file[,i])
+      }
+      
+      ### combine some redundant time points to one
+      if(length(which(colnames(target_file) == "GMP-redo")) > 0) {
+        target_file[,"GMP"] <- target_file[,"GMP"] + target_file[,"GMP-redo"]
+        target_file <- target_file[,-which(colnames(target_file) == "GMP-redo")]
+      }
+      if(length(which(colnames(target_file) == "PreTransB")) > 0) {
+        if(length(which(colnames(target_file) == "PreTrans")) > 0) {
+          target_file[,"PreTrans"] <- target_file[,"PreTrans"] + target_file[,"PreTransB"]
+          target_file <- target_file[,-which(colnames(target_file) == "PreTransB")]
+        } else {
+          colnames(target_file)[which(colnames(target_file) == "PreTransB")] <- "PreTrans"
+        }
+      }
+      if(length(which(colnames(target_file) == "Wk1b")) > 0) {
+        if(length(which(colnames(target_file) == "Wk1")) > 0) {
+          target_file[,"Wk1"] <- target_file[,"Wk1"] + target_file[,"Wk1b"]
+          target_file <- target_file[,-which(colnames(target_file) == "Wk1b")]
+        } else {
+          colnames(target_file)[which(colnames(target_file) == "Wk1b")] <- "Wk1"
+        }
+      }
+      if(length(which(colnames(target_file) == "Wk-1Run1")) > 0) {
+        if(length(which(colnames(target_file) == "Wk-1")) > 0) {
+          target_file[,"Wk-1"] <- target_file[,"Wk-1"] + target_file[,"Wk-1Run1"]
+          target_file <- target_file[,-which(colnames(target_file) == "Wk-1Run1")]
+        } else {
+          colnames(target_file)[which(colnames(target_file) == "Wk-1Run1")] <- "Wk-1"
+        }
+      }
+      if(length(which(colnames(target_file) == "Wk-1Run2")) > 0) {
+        if(length(which(colnames(target_file) == "Wk-1")) > 0) {
+          target_file[,"Wk-1"] <- target_file[,"Wk-1"] + target_file[,"Wk-1Run2"]
+          target_file <- target_file[,-which(colnames(target_file) == "Wk-1Run2")]
+        } else {
+          colnames(target_file)[which(colnames(target_file) == "Wk-1Run2")] <- "Wk-1"
+        }
+      }
+      
+      ### remove all zero time points
+      time_points <- colnames(target_file)[which(apply(target_file, 2, sum) != 0)]
+      
+      ### get time points except the Total
+      time_points <- setdiff(time_points, c("Total"))
+      
+      ### draw when there are at least two time points
+      if(length(time_points) > 1) {
+        ###  get lineages
+        lineage_table <- target_file[which(apply(target_file[,time_points], 1, function(x) {
+          return(length(which(x > 0)) > 1)  
+        })),time_points]
+        lineages <- rownames(lineage_table)
+        
+        ### get gmp lineages
+        gmp_lineage_table <- lineage_table[which(lineage_table$GMP > 0),]
+        gmp_lineages <- rownames(gmp_lineage_table)
+        
+        ### combine lineages
+        if(length(lineages) > 0) {
+          ### combine
+          if(is.null(total_lineages[[type]])) {
+            total_lineages[[type]] <- lineages
+          } else {
+            total_lineages[[type]] <- c(total_lineages[[type]], lineages)
+          }
+        }
+        
+        ### combine gmp lineages
+        if(length(gmp_lineages) > 0) {
+          ### combine
+          if(is.null(total_gmp_lineages[[type]])) {
+            total_gmp_lineages[[type]] <- gmp_lineages
+          } else {
+            total_gmp_lineages[[type]] <- c(total_gmp_lineages[[type]], gmp_lineages)
+          }
+        }
+      }
+      
+      gc()
+    }
+  }
+  
+  ### some pre-calculated indices
+  cd4_carpos_idx <- intersect(which(Seurat_Obj@meta.data$CD4_CD8_by_Consensus == "CD4"),
+                              which(Seurat_Obj@meta.data$CAR == "CARpos"))
+  cd4_carneg_idx <- intersect(which(Seurat_Obj@meta.data$CD4_CD8_by_Consensus == "CD4"),
+                              which(Seurat_Obj@meta.data$CAR == "CARneg"))
+  cd8_carpos_idx <- intersect(which(Seurat_Obj@meta.data$CD4_CD8_by_Consensus == "CD8"),
+                              which(Seurat_Obj@meta.data$CAR == "CARpos"))
+  cd8_carneg_idx <- intersect(which(Seurat_Obj@meta.data$CD4_CD8_by_Consensus == "CD8"),
+                              which(Seurat_Obj@meta.data$CAR == "CARneg"))
+  lineages_idx <- vector("list", length = length(total_lineages))
+  names(lineages_idx) <- names(total_lineages)
+  for(type in names(lineages_idx)) {
+    lineages_idx[[type]] <- which(Seurat_Obj@meta.data[,clonotype_types[type]] %in% total_lineages[[type]])
+  }
+  gmp_lineages_idx <- vector("list", length = length(total_gmp_lineages))
+  names(gmp_lineages_idx) <- names(total_gmp_lineages)
+  for(type in names(gmp_lineages_idx)) {
+    gmp_lineages_idx[[type]] <- which(Seurat_Obj@meta.data[,clonotype_types[type]] %in% total_gmp_lineages[[type]])
+  }
+  
+  ### fill out the table
+  for(i in 1:length(table_colnames)) {
+    for(j in 1:length(unique(Seurat_Obj@meta.data$px))) {
+      
+      ### fill out for each column and for each patient
+      result_table[paste(unique(Seurat_Obj@meta.data$px)[j], "CD4+/CAR+ Clonotype #"),
+                   table_colnames[i]] <- length(unique())
+      
+    }
+  }
   
   
   
